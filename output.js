ef09d69f4358a0dae38136a2f99db66146d5176b
gohilgautam <gohilgautam2405@gmail.com>
2024-11-19 18:47:48 +0530

    output.js - 19-11-2024 - gohil gautam

---
+++ output.js
@@ -0,0 +1,26 @@
+/*/
+    OUTPUT METHOD
+    1.concole.log()
+    2.alert()
+    3.document.write()
+    4.Dom => id
+
+*/
+
+// 1.console.log()
+
+console.error("Answer : ", 2+5);
+console.log("Hello, World!");
+console.warn("Red & White");
+
+// 2. alert()
+
+alert('Gohil Gautam')
+
+// 3.document.write()
+
+document.write('Hello Friends How are you?')
+
+//Dom => id
+
+document.getElementById('pera').textContent = "helloo world";
