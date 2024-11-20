6d6a7ff3425957248bb177a82cc67ea1472b59ac
gohilgautam <gohilgautam2405@gmail.com>
2024-11-20 18:25:23 +0530

    20-11-2024 (Gohil Gautam)

---
+++ datatype.js
@@ -0,0 +1,36 @@
+// VARIABLE :-  
+            //   Variable is like a memory location, 
+            //   variable like container store to data                
+
+
+// initlization
+// var a;
+
+// let const
+
+// let x;
+// const PI=3.14;          //assign
+
+// datatype :- int,char,float/Boolean,string,
+
+/*
+
+datatype:- primitives/nonprimitives
+
+    primitives:-
+            -number
+            -string
+            -boolean
+            -null
+            -undifined
+            -symbol
+            -bigInt
+
+    nonprimitive:-
+            -object
+*/
+
+var num = true;
+
+console.log(num)
+console.log(typeof num)
