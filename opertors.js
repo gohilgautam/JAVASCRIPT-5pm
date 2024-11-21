358eb7a957d202778fe17a394d7cee6959fa5d3e
gohilgautam <gohilgautam2405@gmail.com>
2024-11-21 18:37:55 +0530

    21/11/2024 (Gohil Gautam)

---
+++ opertors.js
@@ -0,0 +1,46 @@
+/*
+
+operators
+
+A+B
+oprands/operators => + => operators
+                  => A+B => oprands
+
+oprands + operators = Expression
+
+*/
+
+// Arithmetic operators :- + , - , * , / , % , ++ , -- , **
+
+// let x=10 , y=3
+
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+// console.log(x++)
+// console.log(x--)
+// console.log(x**y)
+
+//assignment operators :- += , -= , *= , /= , %= , **=
+
+// let x=7 , y=6
+
+// console.log(x += y)
+// console.log(x -= y)
+// console.log(x *= y)
+// console.log(x /= y)
+// console.log(x %= y)
+// console.log(x **= y)
+
+
+// Comparison operators : == , != , > , < , >= , <= , === , !== ,
+
+// logiacl operators :- && , || , !
+
+// let x=0 , y=1
+
+// console.log(x && y)
+// console.log(x || y)
+// console.log(!x)
