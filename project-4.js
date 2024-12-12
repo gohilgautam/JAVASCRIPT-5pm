9e52365007edc5fc8c500f4cbbe14e238072c84d
gohilgautam <gohilgautam2405@gmail.com>
2024-12-12 18:48:40 +0530

    12/12/2024 (Gohil Gautam)

---
+++ project-4.js
@@ -0,0 +1,186 @@
+// 1. write a javascript program to print greeting message using with argument no return type function when call a function without argument at that time print message good morning.
+
+// function Greeting(name) {
+//     if (name === undefined) {
+//       console.log("Good Morning");
+//     } else {
+//       console.log("Good Morning", name);
+//     }
+//   }
+// Greeting();
+// Greeting("hello");
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 2. write a javascript program to find circle area (area = pi*r*r) using return type with argument function.
+
+// let r = +prompt("Enter redius");
+// function a(r) {
+//     let area = 3.14 * r * r
+//         return area;
+// }
+// console.log("area is a :- ", a(r));
+
+/*
+answer :- radius : 20
+          area is a :- 1256
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 3. write a javascript program to find a triangle area (area(l*h/2)) using return type with argument function.
+
+// let l = +prompt("Enter length");
+//         let h = Number(prompt("Enter height"));
+//         function a(l, h) {
+//             let area = ((l * h) / 2)
+//             return area;
+//         }
+// console.log("area is a :- ", a(l, h));
+
+/*
+answer :- length : 20
+          height : 30  
+          area is a : 300
+*/
+        
+// --------------------------------------------------------------------------------------------------------------------
+
+// 4. write a javascript program to find a rectangle area (area l*h) using return type with argument function.
+
+// let l = +prompt("Enter length");
+// let h = Number(prompt("Enter height"));
+//     function b(l, h) {
+//         let area = l * h
+//             return area;
+//         }
+// console.log("area is a :-" , b(l, h));
+
+/*
+answer :- length : 20
+          height : 30  
+          area is a : 600
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 5. write a javascript program to find ansewr of ((b*b)(4*a*c)/(2*a)) using return type with argument function.
+
+// let a = +prompt("Enter value for a");
+// let b = +prompt("Enter value for b");
+// let c = +prompt("Enter value for c");
+//    function d(a, b, c) {
+//        let ans = (((b * b) * (4 * a * c)) / (2 * a))
+//        return ans;
+//  }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d(a, b, c));
+
+/*
+answer :- a = 10
+          b = 10
+          c = 10  
+final answer is :- 2000
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 6. write a javascript program to find answer of (a*a) + (2*a*b) +(b*b) using return type with argument function.
+
+// let a = +prompt("Enter value for a");
+// let b = +prompt("Enter value for b");
+//     function d(a, b) {
+//         let ans = ((a * a) + (2 * a * b) + (b * b))
+//         return ans;
+//     }
+// console.log(a);
+// console.log(b);
+// console.log(d(a, b));
+
+/*
+
+answer :- a = 20
+          b = 30
+final answer is :- 2500
+
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 7. write a javascript program to convert farenhit to celcius (c=(f-32)/1.8 + 32) using return type with argument function.
+
+// let a = +prompt("Enter number to convert farenhit to celcius");
+// function fahrenheait(b) {
+//     let c = a - 32 / 1.8;
+//     return c;
+// }
+// console.log(fahrenheait(a)); 
+
+/*
+
+answer :- fahrenheait = 69
+          celcius = 51.222
+
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 8. write a javascript program to convert celcius to farenhit (c=(f-32)/1.8 + 32) using return type with argument function.
+
+// let a = +prompt("Enter number to convert celcius to farenhit");
+//     function temperature(b) {
+//         let f = a * 1.8 + 32;
+//         return f;
+// }
+// console.log(temperature(a));
+
+/*
+
+answer :- celcius = 69
+          farenhit = 156.2
+
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 9. write a javascript program to find if a given number is even or odd using an argument with no return type.
+
+// let num = +prompt("Enter number");
+// function odd(a) {
+//     if (a % 2 == 0) {
+//         console.log("Given number is even");
+//     }
+//     else {
+//         console.log("Given number is odd");
+//     }
+// }
+// odd(num);
+
+/*
+answer :- 30
+    Given number is even
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// 10. write a javascript program to swap a value without third variable using with argument no teturn type.
+
+// function fine(a, b) {
+//     console.log("Before swapping: a =", a, " b =", b);
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     console.log("After swapping: a =", a, " b =", b);
+// }
+// fine(103, 35);
+
+/*
+Before swapping: a = 103  b = 35
+After swapping: a = 35  b = 103
+*/
+
